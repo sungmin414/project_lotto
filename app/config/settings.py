0616879c6514683fc368ff7883bf47c44fb5bd70
sungmin414 <sungmin3231@gmail.com>
2019-01-08 04:25:13 +0900

    lotto 만들기, urls, views, model, admin, template, form 코딩'

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lotto.apps.LottoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
--- app/config/urls.py
+++ app/config/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from lotto import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lotto/', views.index, name='lotto'),
+    path('', views.index, name='index'),
+    path('lotto/new/', views.post, name='new_lotto'),
+    path('lotto/<int:lottokey>/detail/', views.detail, name='detail')
 ]
