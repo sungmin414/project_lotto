244da61b1f1b7196d0cdc20c68eb2974be1e71f8
sungmin414 <sungmin3231@gmail.com>
2019-01-09 16:02:27 +0900

    views 수정 templates작성

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('Hello, CodeSquad')
+    latest_question_list = Question.objects.order_by('-pub_date')[0:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context )
 
 
 def results(request, question_id):
