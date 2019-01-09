e567a7dbbf782de20bee567991be7eca7a5496e8
sungmin414 <sungmin3231@gmail.com>
2019-01-09 15:27:28 +0900

    views, urls추가

--- app/polls/views.py
+++ app/polls/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Hello, CodeSquad')
+
+
+def results(request, question_id):
+    response = "Question id: %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Vote Page: %s" % question_id)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
