85d13820e93f54dfe9ffc50e7c40921b516c7cc7
sungmin414 <sungmin3231@gmail.com>
2019-01-10 17:19:54 +0900

    admin customize

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -3,5 +3,25 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
